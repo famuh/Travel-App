18cc54797b4ff0caad902d6620b4a3b69b1f5cbc
Fadhil Muhammad <famuh79@gmail.com>
2022-05-05 12:40:29 +0700

    top bar

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -10,6 +10,45 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: [
+              Container(
+                height: 57.6,
+                margin: EdgeInsets.fromLTRB(28.8, 28.8, 28.8, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //custom navigation drawer and search button
+                    Container(
+                      height: 57.6,
+                      width: 57.6,
+                      padding: EdgeInsets.all(18),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(9.6),
+                        color: Color.fromARGB(255, 226, 226, 226),
+                      ),
+                      child: Icon(Icons.list)),
+
+                    Container(
+                      height: 57.6,
+                      width: 57.6,
+                      padding: EdgeInsets.all(18),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(9.6),
+                        color: Color.fromARGB(255, 226, 226, 226),
+                      ),
+                      child: Icon(Icons.search)),
+                  ],
+                ),
+              ),
+              
+            ],
+          ),
+        )),
+    );
   }
 }

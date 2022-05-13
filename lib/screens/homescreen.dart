10ce790b455a3f177139ae8692bfc958faad294a
Fadhil Muhammad <famuh79@gmail.com>
2022-05-13 09:59:07 +0700

    custom tab-bar | recomend, popular, new destination, hidden gems

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -12,18 +13,19 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: ListView(
-            physics: BouncingScrollPhysics(),
-            children: [
-              Container(
-                height: 57.6,
-                margin: EdgeInsets.fromLTRB(28.8, 28.8, 28.8, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //custom navigation drawer and search button
-                    Container(
+          child: Container(
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            //custom navigation drawer and search btn
+            Container(
+              height: 57.6,
+              margin: EdgeInsets.fromLTRB(28.8, 28.8, 28.8, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //custom navigation drawer and search button
+                  Container(
                       height: 57.6,
                       width: 57.6,
                       padding: EdgeInsets.all(18),
@@ -33,7 +35,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                       child: Icon(Icons.list)),
 
-                    Container(
+                  Container(
                       height: 57.6,
                       width: 57.6,
                       padding: EdgeInsets.all(18),
@@ -42,13 +44,40 @@ class _HomeScreenState extends State<HomeScreen> {
                         color: Color.fromARGB(255, 226, 226, 226),
                       ),
                       child: Icon(Icons.search)),
-                  ],
-                ),
+                ],
               ),
-              
-            ],
-          ),
-        )),
+            ),
+
+            //text widget for title
+            Padding(
+                padding: EdgeInsets.only(top: 48, left: 28.8),
+                child: Text('Explore\nThe Nature',
+                    style: GoogleFonts.playfairDisplay(
+                        //playfair itu nama font
+                        fontSize: 45.6,
+                        fontWeight: FontWeight.w700))),
+
+            //custom tab  bar with custom indicator
+            Container(
+              height: 30,
+              margin: EdgeInsets.only(top: 28.8, left: 14.4),
+              child: DefaultTabController(
+                length: 4,
+                child: TabBar(labelPadding: EdgeInsets.symmetric(horizontal: 14.4),tabs: [
+                  Tab(
+                    child: Container(child: Text('Recommend'))),
+                  Tab(
+                    child: Container(child: Text('Popular'))),
+                  Tab(
+                    child: Container(child: Text('New Destination'))),
+                  Tab(
+                    child: Container(child: Text('Hidden Gems'))),
+                ]),
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
